f6346a1d50d31908079e655660ef953597e976a8
rickbacci <rickbacci@gmail.com>
2020-05-09 17:18:40 -0600

    add edit link to shelters index

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Shelter Update' do
   describe 'As a visitor' do
-    describe 'When I visit the shelter show page' do
+    describe 'when I visit the shelter show page' do
       it 'I can update the shelter' do
         shelter = Shelter.create(name: 'foo shelter')
         visit "/shelters/#{shelter.id}"
@@ -10,6 +10,25 @@ RSpec.describe 'Shelter Update' do
         expect(page).to have_link ('Update Shelter')
 
         click_link ('Update Shelter')
+
+        fill_in 'Name', with: 'foobar shelter'
+
+        click_on 'Update Shelter'
+
+        expect(current_path).to eq("/shelters/#{shelter.id}")
+        expect(page).to have_content('foobar shelter')
+      end
+    end
+
+    describe 'when I visit the shelter index page' do
+      it 'I can update the shelter' do
+        shelter = Shelter.create(name: 'Rescue me')
+
+        visit '/shelters'
+
+        expect(page).to have_link('edit')
+
+        find("a\##{shelter.id}").click
 
         fill_in 'Name', with: 'foobar shelter'
 
