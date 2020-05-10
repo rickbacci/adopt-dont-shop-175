db92052ecb1745d1a9b8d78e62760e3df97b8e00
rickbacci <rickbacci@gmail.com>
2020-05-10 13:05:04 -0600

    remove comment

--- spec/features/pets/edit_spec.rb
+++ spec/features/pets/edit_spec.rb
@@ -1,17 +1,3 @@
-
-# When I visit a Pet Show page
-# Then I see a link to update that Pet "Update Pet"
-# When I click the link
-# I am taken to '/pets/:id/edit' where I see a form to edit the pet's data including:
-# - image
-# - name
-# - description
-# - approximate age
-# - sex
-# When I click the button to submit the form "Update Pet"
-# Then a `PATCH` request is sent to '/pets/:id',
-# the pet's data is updated,
-# and I am redirected to the Pet Show page where I see the Pet's updated information
 require 'rails_helper'
 
 RSpec.describe 'Pet Update', type: :feature do
