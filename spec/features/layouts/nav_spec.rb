9804a01b52a135651ac5122109ec247c2a2f8383
rickbacci <rickbacci@gmail.com>
2020-05-09 19:51:45 -0600

    add nav link to shelters index

--- spec/features/layouts/nav_spec.rb
+++ spec/features/layouts/nav_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   context 'when I visit any page on the site' do
-    it 'at the top of the page, I see a link to the Pet Index page' do
+    it 'at the top of the page, I see a link to the Pets Index page' do
       shelter = Shelter.create(name: 'foo')
       shelter.pets.create(name: 'pet')
 
@@ -13,6 +13,19 @@ RSpec.describe 'As a visitor' do
       end
 
       expect(current_path).to eq('/pets')
+    end
+
+    it 'at the top of the page, I see a link to the Shelters Index page' do
+      shelter = Shelter.create(name: 'foo')
+      shelter.pets.create(name: 'pet')
+
+      visit '/pets'
+
+      within('nav') do
+        click_link 'Shelters'
+      end
+
+      expect(current_path).to eq('/shelters')
     end
   end
 end
